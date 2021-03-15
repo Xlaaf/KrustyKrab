3b9038f5642f01177f12fb14195321ac3908a996
farihdzaky <79653902+farihdzaky@users.noreply.github.com>
2021-03-15 22:45:13 +0700

    Update myanimelist.py

--- SaitamaRobot/modules/myanimelist.py
+++ SaitamaRobot/modules/myanimelist.py
@@ -192,7 +192,7 @@ Get information about anime, manga or characters with the help of this module! A
  - /upcoming: returns a list of new anime in the upcoming seasons.
  """
 
-__mod_name__ = "MyAnimeList"
+__mod_name__ = "Anime"
         
         
 ANIME_HANDLER = CommandHandler("anime", anime, pass_args=True)
