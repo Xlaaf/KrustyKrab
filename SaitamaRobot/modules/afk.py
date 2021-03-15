16f6152266f46a0d33a319db0b4d3daa813aeaeb
farihdzaky <79653902+farihdzaky@users.noreply.github.com>
2021-03-15 22:54:52 +0700

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} Go Slash the Titans!".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,7 +57,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
+                '{} I thought you died eating titans', '{} is back!', '{} is now in the chat!',
                 '{} is awake!', '{} is back online!', '{} is finally here!',
                 'Welcome back! {} here you krabby!', 'Where is {}?\nIn the chat!'
             ]
@@ -129,7 +129,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Going to Slash the Titans!.\nAnd the reason: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason))
             update.effective_message.reply_text(res, parse_mode="html")
 
