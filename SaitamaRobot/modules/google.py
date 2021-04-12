fa47875f65fa75805e8575ef7d646ed1fe1ab62f
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-12 23:20:06 +0700

    Update google.py

--- SaitamaRobot/modules/google.py
+++ SaitamaRobot/modules/google.py
@@ -5,7 +5,7 @@ from telethon import *
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
-from Saitamarobot.event import register
+from SaitamaRobot.event import register
 
 
 async def is_register_admin(chat, user):
