b0e0970358782e7b4d073dc3fc33f7b268d0d8ab
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-14 23:14:51 +0700

    Update admin.py

--- Spongebob/modules/admin.py
+++ Spongebob/modules/admin.py
@@ -5,18 +5,18 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
+from Spongebob import DRAGONS, dispatcher
+from Spongebob.modules.disable import DisableAbleCommandHandler
+from Spongebob.modules.helper_funcs.chat_status import (bot_admin, can_pin,
                                                            can_promote,
                                                            connection_status,
                                                            user_admin,
                                                            ADMIN_CACHE)
 
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_user,
+from Spongebob.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
+from Spongebob.modules.log_channel import loggable
+from Spongebob.modules.helper_funcs.alternate import send_message
 
 
 @run_async
