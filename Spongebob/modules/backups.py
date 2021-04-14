cf53bc5548800705d96c161c07ffbbe0c0c38da2
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-14 23:20:32 +0700

    Update backups.py

--- Spongebob/modules/backups.py
+++ Spongebob/modules/backups.py
@@ -5,23 +5,23 @@ from telegram import ParseMode, Message
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER
-from SaitamaRobot.__main__ import DATA_IMPORT
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.alternate import typing_action
+import Spongebob.modules.sql.notes_sql as sql
+from Spongebob import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER
+from Spongebob.__main__ import DATA_IMPORT
+from Spongebob.modules.helper_funcs.chat_status import user_admin
+from Spongebob.modules.helper_funcs.alternate import typing_action
 
-# from SaitamaRobot.modules.rules import get_rules
-import SaitamaRobot.modules.sql.rules_sql as rulessql
+# from Spongebob.modules.rules import get_rules
+import Spongebob.modules.sql.rules_sql as rulessql
 
-# from SaitamaRobot.modules.sql import warns_sql as warnssql
-import SaitamaRobot.modules.sql.blacklist_sql as blacklistsql
-from SaitamaRobot.modules.sql import disable_sql as disabledsql
+# from Spongebob.modules.sql import warns_sql as warnssql
+import Spongebob.modules.sql.blacklist_sql as blacklistsql
+from Spongebob.modules.sql import disable_sql as disabledsql
 
-# from SaitamaRobot.modules.sql import cust_filters_sql as filtersql
-# import SaitamaRobot.modules.sql.welcome_sql as welcsql
-import SaitamaRobot.modules.sql.locks_sql as locksql
-from SaitamaRobot.modules.connection import connected
+# from Spongebob.modules.sql import cust_filters_sql as filtersql
+# import Spongebob.modules.sql.welcome_sql as welcsql
+import Spongebob.modules.sql.locks_sql as locksql
+from Spongebob.modules.connection import connected
 
 
 @run_async
