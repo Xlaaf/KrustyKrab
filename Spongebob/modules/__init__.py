7d4809cfa1afaab09e3fbba59f9f9fa7c1cc76f6
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-14 23:03:37 +0700

    Add files via upload

--- Spongebob/modules/__init__.py
+++ Spongebob/modules/__init__.py
@@ -1,4 +1,4 @@
-from Spongebob import LOAD, LOGGER, NO_LOAD
+from SaitamaRobot import LOAD, LOGGER, NO_LOAD
 
 
 def __list_all_modules():
---
+++ Spongebob/modules/kickthefools.py
@@ -0,0 +1,38 @@
+from telethon.tl.types import UserStatusLastMonth, UserStatusLastWeek, ChatBannedRights
+from SaitamaRobot.event import register
+from telethon import *
+from telethon.tl.functions.channels import (EditBannedRequest)
+                                            
+@register(pattern="^/kickthefools")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not event.chat.admin_rights.ban_users:
+        return
+    if not admin and not creator:
+        await event.reply("I am not admin here !")
+        return
+    c = 0
+    KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
+    await event.reply("Searching Participant Lists...")
+    async for i in event.client.iter_participants(event.chat_id):
+
+        if isinstance(i.status, UserStatusLastMonth):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1
+                    
+        if isinstance(i.status, UserStatusLastWeek):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1                    
+
+    required_string = "Successfully Kicked **{}** users"
+    await event.reply(required_string.format(c))
