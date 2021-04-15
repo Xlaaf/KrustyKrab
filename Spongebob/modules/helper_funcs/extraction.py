dd4ed751f0b07b2f5bd66e433c17a9eb54e3338d
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:15:47 +0700

    Update extraction.py

--- Spongebob/modules/helper_funcs/extraction.py
+++ Spongebob/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SaitamaRobot import LOGGER
-from SaitamaRobot.modules.users import get_user_id
+from Spongebob import LOGGER
+from Spongebob.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
