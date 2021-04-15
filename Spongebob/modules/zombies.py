e4bed6e961925831e71b74dbcd168299b8f9bd95
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 08:04:29 +0700

    Update zombies.py

--- Spongebob/modules/zombies.py
+++ Spongebob/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from SaitamaRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, TIGERS
+from Spongebob import telethn, OWNER_ID, DEV_USERS, DRAGONS, TIGERS
 
 # =================== CONSTANT ===================
 
