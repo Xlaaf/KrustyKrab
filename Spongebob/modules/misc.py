c10a692545adea74e7daf1bf161292d38bf5fad2
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:36:53 +0700

    Update misc.py

--- Spongebob/modules/misc.py
+++ Spongebob/modules/misc.py
@@ -1,6 +1,6 @@
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher
+from Spongebob.modules.helper_funcs.chat_status import user_admin
+from Spongebob.modules.disable import DisableAbleCommandHandler
+from Spongebob import dispatcher
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import ParseMode, Update
