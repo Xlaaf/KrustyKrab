df4038e5fd8e369287ff1120597d4d3b18f66a5a
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:38:44 +0700

    Update notes.py

--- Spongebob/modules/notes.py
+++ Spongebob/modules/notes.py
@@ -3,15 +3,15 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from SaitamaRobot.modules.helper_funcs.misc import (build_keyboard,
+import Spongebob.modules.sql.notes_sql as sql
+from Spongebob import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from Spongebob.modules.disable import DisableAbleCommandHandler
+from Spongebob.modules.helper_funcs.handlers import MessageHandlerChecker
+from Spongebob.modules.helper_funcs.chat_status import user_admin, connection_status
+from Spongebob.modules.helper_funcs.misc import (build_keyboard,
                                                     revert_buttons)
-from SaitamaRobot.modules.helper_funcs.msg_types import get_note_type
-from SaitamaRobot.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
+from Spongebob.modules.helper_funcs.msg_types import get_note_type
+from Spongebob.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
 from telegram import (MAX_MESSAGE_LENGTH, InlineKeyboardMarkup, Message,
                       ParseMode, Update, InlineKeyboardButton)
 from telegram.error import BadRequest
