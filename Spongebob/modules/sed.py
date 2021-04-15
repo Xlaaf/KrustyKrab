c7c76832edf8172be0f76b986238d4ccc4b23ba3
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:58:27 +0700

    Update sed.py

--- Spongebob/modules/sed.py
+++ Spongebob/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleMessageHandler
-from SaitamaRobot.modules.helper_funcs.regex_helper import (infinite_loop_check)
+from Spongebob import LOGGER, dispatcher
+from Spongebob.modules.disable import DisableAbleMessageHandler
+from Spongebob.modules.helper_funcs.regex_helper import (infinite_loop_check)
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
