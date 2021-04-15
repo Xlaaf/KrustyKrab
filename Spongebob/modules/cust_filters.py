8abda5ea1e27b7c64880a32fe5dc6e8a976ca19a
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:24:07 +0700

    Update cust_filters.py

--- Spongebob/modules/cust_filters.py
+++ Spongebob/modules/cust_filters.py
@@ -15,25 +15,25 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html, escape_markdown
 
-from SaitamaRobot import dispatcher, LOGGER, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard_parser
-from SaitamaRobot.modules.helper_funcs.msg_types import get_filter_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from Spongebob import dispatcher, LOGGER, DRAGONS
+from Spongebob.modules.disable import DisableAbleCommandHandler
+from Spongebob.modules.helper_funcs.handlers import MessageHandlerChecker
+from Spongebob.modules.helper_funcs.chat_status import user_admin
+from Spongebob.modules.helper_funcs.extraction import extract_text
+from Spongebob.modules.helper_funcs.filters import CustomFilters
+from Spongebob.modules.helper_funcs.misc import build_keyboard_parser
+from Spongebob.modules.helper_funcs.msg_types import get_filter_type
+from Spongebob.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from SaitamaRobot.modules.sql import cust_filters_sql as sql
+from Spongebob.modules.sql import cust_filters_sql as sql
 
-from SaitamaRobot.modules.connection import connected
+from Spongebob.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Spongebob.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
