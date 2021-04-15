eb578993b6e0cfdefb1b12aca76e4b0641c3b229
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:32:01 +0700

    Update gettime.py

--- Spongebob/modules/gettime.py
+++ Spongebob/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Spongebob import TIME_API_KEY, dispatcher
+from Spongebob.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
