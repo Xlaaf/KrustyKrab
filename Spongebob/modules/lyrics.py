a254a6ff828054f2ae9f83bdd9461ee2659ade56
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:36:04 +0700

    Update lyrics.py

--- Spongebob/modules/lyrics.py
+++ Spongebob/modules/lyrics.py
@@ -3,8 +3,8 @@ from tswift import Song
 from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Spongebob import dispatcher
+from Spongebob.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
