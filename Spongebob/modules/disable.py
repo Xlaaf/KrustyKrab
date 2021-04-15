957c9dc9225c147dd125a5fea241ef8608fc2b52
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:26:12 +0700

    Update disable.py

--- Spongebob/modules/disable.py
+++ Spongebob/modules/disable.py
@@ -2,10 +2,10 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.handlers import (CMD_STARTERS,
+from Spongebob import dispatcher
+from Spongebob.modules.helper_funcs.handlers import (CMD_STARTERS,
                                                         SpamChecker)
-from SaitamaRobot.modules.helper_funcs.misc import is_module_loaded
+from Spongebob.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, RegexHandler)
@@ -16,9 +16,9 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from SaitamaRobot.modules.helper_funcs.chat_status import (
+    from Spongebob.modules.helper_funcs.chat_status import (
         connection_status, is_user_admin, user_admin)
-    from SaitamaRobot.modules.sql import disable_sql as sql
+    from Spongebob.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
