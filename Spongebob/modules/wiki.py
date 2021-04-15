93d99f4bb832d73da8408818ffd3ad9f3cbcac3e
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 08:03:50 +0700

    Update wiki.py

--- Spongebob/modules/wiki.py
+++ Spongebob/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Spongebob import dispatcher
+from Spongebob.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
