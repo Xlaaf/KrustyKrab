120a3e4f683d55cb2b9500fe402ea8dc0711483e
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:05:35 +0700

    Update blacklist_sql.py

--- Spongebob/modules/sql/blacklist_sql.py
+++ Spongebob/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from SaitamaRobot.modules.sql import SESSION, BASE
+from Spongebob.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
