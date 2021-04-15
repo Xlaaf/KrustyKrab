3c443810e4c120516604f7d7a7b48cec45144945
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 07:35:03 +0700

    Update locks.py

--- Spongebob/modules/locks.py
+++ Spongebob/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import SaitamaRobot.modules.sql.locks_sql as sql
-from SaitamaRobot import dispatcher, DRAGONS, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+import Spongebob.modules.sql.locks_sql as sql
+from Spongebob import dispatcher, DRAGONS, LOGGER
+from Spongebob.modules.disable import DisableAbleCommandHandler
+from Spongebob.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.sql.approve_sql import is_approved
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Spongebob.modules.log_channel import loggable
+from Spongebob.modules.connection import connected
+from Spongebob.modules.sql.approve_sql import is_approved
+from Spongebob.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
