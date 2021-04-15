f7777abfbf57f09c8215de771a040ec1e492d2fb
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-15 08:03:14 +0700

    Update welcome.py

--- Spongebob/modules/welcome.py
+++ Spongebob/modules/welcome.py
@@ -4,21 +4,21 @@ import re
 import time
 from functools import partial
 
-import SaitamaRobot.modules.sql.welcome_sql as sql
-from SaitamaRobot import (DEV_USERS, LOGGER, OWNER_ID, DRAGONS, DEMONS, TIGERS,
+import Spongebob.modules.sql.welcome_sql as sql
+from Spongebob import (DEV_USERS, LOGGER, OWNER_ID, DRAGONS, DEMONS, TIGERS,
                           WOLVES, sw, dispatcher, JOIN_LOGGER)
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from Spongebob.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_welcome_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from Spongebob.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from Spongebob.modules.helper_funcs.msg_types import get_welcome_type
+from Spongebob.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
+from Spongebob.modules.log_channel import loggable
+from Spongebob.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
